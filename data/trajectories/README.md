c62a852aa839e984986fc786808bc68dd1be8d72
Thomas Gossard <thomas.gossard@uni-tuebingen.de>
2023-04-14 10:27:55 +0200

    Added cleaned dataset

--- data/trajectories/README.md
+++ data/trajectories/README.md
@@ -1,12 +1,17 @@
 # Table tennis ball trajectory dataset 
 
-Dataset is being refined and checked for outliers, will be available shortly.
 
 An example of a script to read the dataset is available:
 
 ```python
-python read_trajectory.py ---idx 0
+python read_trajectory.py  [index of the trajectory to read]
 ```
 
 
 ## Format
+The frame used to describe the trajectory is shown in the following picture. The unit used is meters for the position and rad/s for the spin. The spin is represented as a vector.
+
+ 
+![table_frame](../../img/table_frame.png)
+
+All the spins are saved in the index.csv file and refer to the corresponding trajectory in another numbered csv file.           
--- data/trajectories/read_trajectory.py
+++ data/trajectories/read_trajectory.py
@@ -1,7 +1,49 @@
 """
 Example script to read and visualize the trajectories
 """
-import h5py
 import argparse
+from pathlib import Path
+
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+
+
+def read_traj(traj_name):
+    traj = np.genfromtxt(
+        "trajectories/{:03d}.csv".format(int(traj_name)), delimiter=";"
+    )
+    return traj
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "idx",
+        type=int,
+        help="Index of the trajectory to read",
+    )
+    args = parser.parse_args()
+
+    # Read the index
+    index_path = Path("trajectories/index.csv")
+    index = pd.read_csv(str(index_path))
+
+    spin = index.loc[args.idx][["x_spin", "y_spin", "z_spin"]]
+    print("Spin vector")
+    print(spin)
+
+    # Read the trajectory
+    traj = read_traj(index.loc[args.idx]["traj_file"])
+
+    # Plot the trajectory
+    fig, axs = plt.subplots(3)
+    axs[0].plot(traj[:, 0], traj[:, 1])
+    axs[1].plot(traj[:, 0], traj[:, 2])
+    axs[2].plot(traj[:, 0], traj[:, 3])
+
+    axs[2].set_xlabel("Time [s]")
+    axs[0].set_ylabel("X [m]")
+    axs[1].set_ylabel("Y [m]")
+    axs[2].set_ylabel("Z [m]")
+    plt.show()
